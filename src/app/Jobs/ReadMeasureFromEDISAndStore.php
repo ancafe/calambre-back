7346417033e10ea2d4343110a3352ddd136eead1
Ángel Cardiel <angel.cardiel@protonmail.com>
2022-05-05 03:10:50 +0200

    Queue implemented... needs to be review. Next step... Rate Limit using Redis!

---
+++ src/app/Jobs/ReadMeasureFromEDISAndStore.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Exceptions\ErrorDtoFactory;
+use App\Exceptions\Type\ApiError;
+use App\Models\Contract;
+use App\Models\EdisInfo;
+use App\Models\Supply;
+use App\Models\User;
+use App\Services\Edis\EdisService;
+use App\Services\Measure\StorageMeasureService;
+use Edistribucion\EdisClient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ReadMeasureFromEDISAndStore implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    protected Contract $contract;
+    protected Supply $supply;
+    protected \DateTime $startDate;
+    protected \DateTime $endDate;
+    protected bool $interval;
+    protected EdisService $edisService;
+    protected StorageMeasureService $storageService;
+    protected User $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        User $user,
+        Contract $contract,
+        Supply $supply,
+        ?\DateTime $startDate = null,
+        ?\DateTime $endDate = null,
+        bool $interval = false
+    )
+    {
+        $this->user = $user->withoutRelations();
+        $this->contract = $contract->withoutRelations();
+        $this->supply = $supply->withoutRelations();
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->interval = $interval;
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Edistribucion\EdisError|ApiError
+     */
+    public function handle(StorageMeasureService $storageService)
+    {
+        $this->storageService = $storageService;
+
+        $this->edisService = new EdisService(
+            new EdisClient(
+                $this->user->edis->username,
+                $this->user->edis->password,
+            )
+        );
+
+        $measures = null;
+
+        if ($this->interval) {
+            $measures = $this->edisService->getMeasureInterval($this->contract->atrId, $this->startDate, $this->endDate);
+        } else {
+            $measures = $this->edisService->getMeasure($this->contract->atrId);
+        }
+
+        if ($measures){
+            $this->storageService->storage($measures, $this->user, $this->supply);
+        } else {
+            throw new ApiError([ErrorDtoFactory::noMeasureFounded()]);
+        }
+    }
+}
---
+++ src/database/migrations/2022_05_04_234249_create_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+
+        DB::statement('GRANT ALL ON TABLE public.jobs TO "RLS_Users";');
+        DB::statement('GRANT ALL ON SEQUENCE public.jobs_id_seq TO "RLS_Users";');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
