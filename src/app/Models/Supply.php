65750dc40e7ac58cf9c30c30a29295a2bd2f7368
Ángel Cardiel <angel.cardiel@protonmail.com>
2022-05-13 16:38:56 +0200

    Get latest data received

--- src/app/Models/Supply.php
+++ src/app/Models/Supply.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use App\Casts\EncrypterCast;
 use App\Traits\UUID;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
 
 class Supply extends Model
 {
@@ -25,6 +27,8 @@ class Supply extends Model
         'provisioning_address' => 'encrypted',
     ];
 
+    protected $appends = ['last_data'];
+
 
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
@@ -36,9 +40,21 @@ class Supply extends Model
         return $this->hasMany(Contract::class, 'supply', 'id');
     }
 
-    public function measure():  \Illuminate\Database\Eloquent\Relations\HasMany
+    public function measure(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Measure::class, 'supply', 'id');
+    }
+
+    public function getLastDataAttribute(): Carbon|null
+    {
+
+        $latest = Measure::where("value", ">", 0)
+            ->where("supply", $this->id)
+            ->orderBy("date", "DESC")
+            ->orderBy("hournum", "DESC")
+            ->first();
+
+        return ($latest) ? $latest->data : null;
     }
 
 
