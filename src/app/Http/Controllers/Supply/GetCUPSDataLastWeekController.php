34e9a8d7c46af81ec4743c8d4906c6fe3559cb58
Ángel Cardiel <angel.cardiel@protonmail.com>
2022-06-01 22:56:59 +0200

    Start of week

--- src/app/Http/Controllers/Supply/GetCUPSDataLastWeekController.php
+++ src/app/Http/Controllers/Supply/GetCUPSDataLastWeekController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Supply;
 use App\Exceptions\Type\ApiError;
 use App\Models\Supply;
 use App\Services\API_Response\APISuccess;
+use Carbon\Carbon;
 use DateTime;
 use Illuminate\Http\JsonResponse;
 
@@ -16,8 +17,10 @@ class GetCUPSDataLastWeekController extends AbstractControllerReturnDataForChart
      */
     public function __invoke(Supply $supply): JsonResponse
     {
-        $from = (new \DateTime)->setTimestamp(strtotime("last week monday"));
-        $to = (new \DateTime)->setTimestamp(strtotime("next sunday", strtotime("last week monday")));
+        $last_data = new Carbon($supply->getLastDataAttribute());
+        $last_data->setISODate($last_data->year,$last_data->weekOfYear);
+        $from = $last_data->copy()->startOfWeek();
+        $to = $last_data->copy()->endOfWeek();
 
         return response()->json(new APISuccess($this->CUPSDataService->bar($supply, $from, $to)));
         //return response()->json(new APISuccess($this->CUPSDataService->get($supply, $from, $to)));
